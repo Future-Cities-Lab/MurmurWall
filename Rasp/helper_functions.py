008cc40c142c724e0724ca1df7138fc6f1e9867d
Collin Schupman <collinschupman.com>
2015-02-19 23:25:24 +0000

    stuff

--- Rasp/helper_functions.py
+++ Rasp/helper_functions.py
@@ -5,7 +5,7 @@ def get_available_ports():
     if platform.system() == "Darwin":
         return glob.glob('/dev/tty.*')
     else:
-        return glob.glob('/dev/tty*')
+        return glob.glob('/dev/tty[A-Za-z]*')
 
 def my_range(start, stop, step):
     while start < stop:
