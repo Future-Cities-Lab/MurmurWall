d1c51be3dbc228a0539e00df2749198d6d35b295
Collin Schupman <collinschupman@Collins-MacBook-Pro.local>
2015-03-16 15:25:36 -0700

    urgh

--- Onsite/Raspi/animation.py
+++ Onsite/Raspi/animation.py
@@ -112,7 +112,7 @@ def update_matrices(led_matrices):
     """
     for led_matrix in led_matrices.values():
         word = led_matrix.check_status()
-        print word
+        #print word
         if word is not '' and 'messed up':
             print word
             for packet in led_matrix.packets:
